300f6dbb5969d9fab57602d24755afba814a5e9a
Sheldon <sheldonaldridge@yahoo.com>
2024-02-25 20:52:00 +0200

    Create mobile response first
    Creating burger menu with toggle animation

---
+++ resources/js/burger-menu.js
@@ -0,0 +1,7 @@
+let burgerMenu = document.querySelector("#burger-menu");
+let nav = document.querySelector(".navbar");
+
+burgerMenu.addEventListener("click",(e) => {
+    nav.style.display="block";
+})
+
